4dfaa1f35d57343fec84bba86d3741d48c092dd7
AnastasiiaKotielnikova <akotelnikova1993@gmail.com>
2022-09-08 11:58:24 +0300

    add hw-11

---
+++ src/js/apiServ.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+export default apiServ;
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const KEY = '29770488-919f6a75902a1a7de70fff114';
+
+
+
+async function apiServ(query, page, perPage) {
+    const response = await axios.get (`?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,)
+    return response;
+}
